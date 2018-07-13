b7ca314a65715fae51d769f180951d0a28e0a087
Andrea Florez Garcia - Ceiba Software <andrea.florez@ceiba.com.co>
2018-07-13 11:29:42 -0500

    induccion

---
+++ src/main/java/com/ceiba/induccionCeiba/InduccionCeibaApplication.java
@@ -0,0 +1,12 @@
+package com.ceiba.induccionCeiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InduccionCeibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InduccionCeibaApplication.class, args);
+	}
+}
